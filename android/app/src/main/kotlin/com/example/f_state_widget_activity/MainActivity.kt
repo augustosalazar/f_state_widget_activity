7ba5cb730b8a54aa8f464c57ee05251225da4ee5
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2025-07-21 17:19:16 -0500

    update to 202530

--- android/app/src/main/kotlin/com/example/f_state_widget_activity/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/f_state_widget_activity/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.f_state_widget_activity
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
